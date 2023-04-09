35e8ec6d9cbfb0461a7627ec92660969bfe92561
lululujojo123 <lustersjohn@gmail.com>
2023-04-09 02:44:31 +0200

    Another try
    Try

--- script.js
+++ script.js
@@ -2,10 +2,30 @@ var currentIndex = 1;
 var clicked = false;
 var loaded = 0;
 
+function loadCallback() {
+    if (loaded < 2) {
+        loaded++;
+    } else {
+        document.getElementsByClassName("view-container")[0].innerHTML = images[currentIndex];
+
+        loaded = 0;
+        clicked = false;
+    }
+}
+
+var firstImage = new Image();
+firstImage.onload = loadCallback;
+
+var gift = new Image();
+gift.onload = loadCallback;
+
+var firstImageAlpha = new Image();
+firstImageAlpha.onload = loadCallback;
+
 var images = [
-    '<img id="first-image-alpha" class="lazy returned" src="./images/44788279211_2e231bd959_k.png" loading="lazy" decoding="async"/>\n'+
-    '<img id="gift" class="lazy returned" src="./images/IMG_20230408_150250529.png" loading="lazy" decoding="async" onclick="found()"/>\n'+
-    '<img id="first-image" class="lazy returned" src="./images/44788279211_2e231bd959_k.jpg" loading="lazy" decoding="async"/>\n',
+    '<img id="first-image-alpha" class="lazy returned" src="./images/44788279211_2e231bd959_k.png"/>\n'+
+    '<img id="gift" class="lazy returned" src="./images/IMG_20230408_150250529.png" onclick="found()"/>\n'+
+    '<img id="first-image" class="lazy returned" src="./images/44788279211_2e231bd959_k.jpg"/>\n',
     '<img id="second-image" src="./images/16529177121_fa5daa3403_b.jpg"/>',
     '<img id="third-image" src="./images/8401710770_7e031f9658_k.jpg"/>'
 ]
@@ -23,9 +43,15 @@ function nextGarden() {
         currentIndex=0;
     }
 
-    document.getElementsByClassName("view-container")[0].innerHTML = images[currentIndex];
-
-    clicked = false;
+    if (currentIndex == 0) {
+        firstImage.src = './images/44788279211_2e231bd959_k.jpg';
+        firstImageAlpha.src = './images/44788279211_2e231bd959_k.png';
+        gift.src = './images/IMG_20230408_150250529.png';
+    } else {
+        document.getElementsByClassName("view-container")[0].innerHTML = images[currentIndex];
+    
+        clicked = false;
+    }
 }
 
 function previousGarden() {
@@ -41,9 +67,15 @@ function previousGarden() {
         currentIndex=2;
     }
 
-    document.getElementsByClassName("view-container")[0].innerHTML = images[currentIndex];
+    if (currentIndex == 0) {
+        firstImage.src = './images/44788279211_2e231bd959_k.jpg';
+        firstImageAlpha.src = './images/44788279211_2e231bd959_k.png';
+        gift.src = './images/IMG_20230408_150250529.png';
+    } else {
+        document.getElementsByClassName("view-container")[0].innerHTML = images[currentIndex];
     
-    clicked = false;
+        clicked = false;
+    }
 }
 
 function found() {
